8a8ecda3d4c99c6d5c8526cccbb412b0572c405c
wanbing <43331024+pashenmebaojinwo@users.noreply.github.com>
2023-05-11 01:24:09 +0800

    使BeanFactory的方法名和spring框架内的保持一致

--- src/main/java/com/wanbing/springframework/beans/factory/BeanFactory.java
+++ src/main/java/com/wanbing/springframework/beans/factory/BeanFactory.java
@@ -5,5 +5,7 @@ import com.wanbing.springframework.beans.exception.BeansException;
 public interface BeanFactory {
     Object getBean(String beanName) throws BeansException;
 
-    void registerBeanDefinition(BeanDefinition beanDefinition);
+    Boolean containsBean(String name);
+
+    void registerBean(String beanName, Object obj);
 }
