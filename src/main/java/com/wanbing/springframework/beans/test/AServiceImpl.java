147a55cb6137936246b8f5acdc18a590204b5e12
wanbing <43331024+pashenmebaojinwo@users.noreply.github.com>
2023-05-16 03:40:29 +0800

    配置文件和测试类

--- src/main/java/com/wanbing/springframework/beans/test/AServiceImpl.java
+++ src/main/java/com/wanbing/springframework/beans/test/AServiceImpl.java
@@ -1,6 +1,20 @@
 package com.wanbing.springframework.beans.test;
 
 public class AServiceImpl implements AService{
+    private String property1;
+
+    private String name;
+
+    private Integer level;
+
+    public AServiceImpl() {
+    }
+
+    public AServiceImpl(String name, Integer level) {
+        this.name = name;
+        this.level = level;
+    }
+
     @Override
     public void sayHello() {
         System.out.println("a service 1 say hello");
--- src/main/java/com/wanbing/springframework/beans/test/Test1.java
+++ src/main/java/com/wanbing/springframework/beans/test/Test1.java
@@ -5,7 +5,7 @@ import com.wanbing.springframework.context.ClassPathXmlApplicationContext;
 
 public class Test1 {
     public static void main(String[] args) throws BeansException {
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("/beans.xml");
         AService aservice = (AService) ctx.getBean("aservice");
         aservice.sayHello();
 
