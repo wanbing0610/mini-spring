918dba14e1030bc91bb890f8e61d87fe794c36cf
wanbing <43331024+pashenmebaojinwo@users.noreply.github.com>
2023-05-16 03:31:34 +0800

    为BeanDefinition增加更能信息，例如 原型 懒加载

--- src/main/java/com/wanbing/springframework/beans/factory/BeanDefinition.java
+++ src/main/java/com/wanbing/springframework/beans/factory/BeanDefinition.java
@@ -1,6 +1,28 @@
 package com.wanbing.springframework.beans.factory;
 
+import com.wanbing.springframework.beans.factory.config.ArgumentValues;
+import com.wanbing.springframework.beans.factory.config.PropertyValues;
+
 public class BeanDefinition {
+
+    /**
+     * 单例
+     */
+    public static String SCOPE_SINGLETON = "singleton";
+
+    public static String SCOPE_PROTOTYPE = "prototype";
+
+    private String scope = SCOPE_SINGLETON;
+
+    private boolean lazyInit = false;
+    private String[] dependsOn;
+    private ArgumentValues constructorArgumentValues;
+
+    private PropertyValues propertyValues;
+    private String initMethodName;
+
+    private volatile Object beanClass;
+
     private String id;
     private String className;
 
@@ -24,4 +46,78 @@ public class BeanDefinition {
     public void setClassName(String className) {
         this.className = className;
     }
+
+    public boolean hasBeanClass() {
+        return (this.beanClass instanceof Class);
+    }
+
+    public void setBeanClass(Class<?> beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public Class<?> getBeanClass(){
+        return (Class<?>) this.beanClass;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public boolean isSingleton() {
+        return SCOPE_SINGLETON.equals(scope);
+    }
+
+    public boolean isPrototype() {
+        return SCOPE_PROTOTYPE.equals(scope);
+    }
+
+    public boolean isLazyInit() {
+        return lazyInit;
+    }
+
+    public void setLazyInit(boolean lazyInit) {
+        this.lazyInit = lazyInit;
+    }
+
+    public String[] getDependsOn() {
+        return dependsOn;
+    }
+
+    public void setDependsOn(String[] dependsOn) {
+        this.dependsOn = dependsOn;
+    }
+
+    public ArgumentValues getConstructorArgumentValues() {
+        return constructorArgumentValues;
+    }
+
+    public void setConstructorArgumentValues(ArgumentValues constructorArgumentValues) {
+        this.constructorArgumentValues = (constructorArgumentValues != null ? constructorArgumentValues : new ArgumentValues());
+    }
+
+    public boolean hasConstructorArgumentValues() {
+        return !this.constructorArgumentValues.isEmpty();
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = (propertyValues != null ? propertyValues : new PropertyValues());
+    }
+
+    public String getInitMethodName() {
+        return initMethodName;
+    }
+
+    public void setInitMethodName(String initMethodName) {
+        this.initMethodName = initMethodName;
+    }
+
+
 }
